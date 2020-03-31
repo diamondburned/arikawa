727f23f8f4064a08894418ee1b5588606b858179
diamondburned (Forefront) <datutbrus@gmail.com>
2020-03-30 22:22:32 -0700

    Gateway: Added missing Activities slice in UpdateStatus

--- gateway/commands.go
+++ gateway/commands.go
@@ -88,8 +88,9 @@ func (g *Gateway) UpdateVoiceState(data UpdateVoiceStateData) error {
 }
 
 type UpdateStatusData struct {
-	Since discord.UnixMsTimestamp `json:"since,omitempty"` // 0 if not idle
-	Game  *discord.Activity       `json:"game,omitempty"`  // nullable
+	Since      discord.UnixMsTimestamp `json:"since,omitempty"` // 0 if not idle
+	Game       *discord.Activity       `json:"game,omitempty"`  // nullable
+	Activities []discord.Activity      `json:"activities,omitempty"`
 
 	Status discord.Status `json:"status"`
 	AFK    bool           `json:"afk"`
