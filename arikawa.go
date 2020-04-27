10bdb19f68ee6ffe6ebb75de43e0229d4fcdbad1
diamondburned (Forefront) <datutbrus@gmail.com>
2020-04-27 11:41:28 -0700

    Added a root package to allow go get

---
+++ arikawa.go
@@ -0,0 +1,41 @@
+// Package arikawa contains a set of modular packages that allows you to make a
+// Discord bot or any type of session (OAuth unsupported).
+//
+// Session
+//
+// Package session is the most simple abstraction, which combines the API
+// package and the Gateway websocket package together into one. This could be
+// used for minimal bots that only use gateway events and such.
+//
+// State
+//
+// Package state abstracts on top of session and provides a local cache of API
+// calls and events. Bots that either don't need a command router or already has
+// its own should use this package.
+//
+// Bot
+//
+// Package bot abstracts on top of state and provides a command router based on
+// Go code. This is similar to discord.py's API, only it's Go and there's no
+// optional arguments (yet, although it could be worked around). Most bots are
+// recommended to use this package, as it's the easiest way to make a bot.
+//
+// Voice
+//
+// Package voice provides an abstraction on top of State and adds voice support.
+// This allows bots to join voice channels and talk. The package uses an
+// io.Writer approach rather than a channel, contrary to other Discord
+// libraries.
+package arikawa
+
+import (
+	// Packages that most should use.
+	_ "github.com/diamondburned/arikawa/bot"
+	_ "github.com/diamondburned/arikawa/session"
+	_ "github.com/diamondburned/arikawa/state"
+	_ "github.com/diamondburned/arikawa/voice"
+
+	// Low level packages.
+	_ "github.com/diamondburned/arikawa/api"
+	_ "github.com/diamondburned/arikawa/gateway"
+)
--- go.mod
+++ go.mod
@@ -9,6 +9,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/sasha-s/go-csync v0.0.0-20160729053059-3bc6c8bdb3fa
 	github.com/sasha-s/go-deadlock v0.2.0
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
+	golang.org/x/crypto v0.0.0-20200423211502-4bdfaf469ed5
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1
 )
