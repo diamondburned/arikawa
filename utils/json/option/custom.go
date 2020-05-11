b30a1fb7d2c4c595b9c1bee61d17c329cf2298c4
mavolin <48887425+mavolin@users.noreply.github.com>
2020-05-11 05:24:12 +0200

    Utils: add custom option types

---
+++ utils/json/option/custom.go
@@ -0,0 +1,22 @@
+package option
+
+import "github.com/diamondburned/arikawa/discord"
+
+// ================================ Seconds ================================
+
+// Seconds is the option type for discord.Seconds.
+type Seconds *discord.Seconds
+
+// ZeroSeconds are 0 Seconds.
+var ZeroSeconds = NewSeconds(0)
+
+// NewString creates a new Seconds with the value of the passed discord.Seconds.
+func NewSeconds(s discord.Seconds) Seconds { return &s }
+
+// ================================ Color ================================
+
+// Color is the option type for discord.Color.
+type Color *discord.Color
+
+// NewString creates a new Color with the value of the passed discord.Color.
+func NewColor(s discord.Color) Color { return &s }
