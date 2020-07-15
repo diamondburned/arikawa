c1885067d706d914cd309e6b9f17c8a30e76dff6
diamondburned <datutbrus@gmail.com>
2020-07-14 18:47:15 -0700

    Gateway: Allow for more lenient gateway bursts

--- utils/wsutil/throttler.go
+++ utils/wsutil/throttler.go
@@ -7,8 +7,7 @@ import (
 )
 
 func NewSendLimiter() *rate.Limiter {
-	// return rate.NewLimiter(rate.Every(time.Minute), 120)
-	return rate.NewLimiter(rate.Every(time.Second), 2)
+	return rate.NewLimiter(rate.Every(time.Minute), 120)
 }
 
 func NewDialLimiter() *rate.Limiter {
