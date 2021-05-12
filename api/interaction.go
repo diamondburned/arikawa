c880cb2fc809cfaa6569e72d8b3df83dc4d070e0
Chan Wen Xu <lutherchanpublic@gmail.com>
2021-05-12 13:36:03 +0800

    interaction: Implement buttons

--- api/interaction.go
+++ api/interaction.go
@@ -15,6 +15,8 @@ const (
 	MessageInteraction
 	MessageInteractionWithSource
 	AcknowledgeInteractionWithSource
+	DeferredMessageUpdate
+	UpdateMessage
 )
 
 type InteractionResponse struct {
@@ -25,17 +27,17 @@ type InteractionResponse struct {
 // InteractionResponseData is InteractionApplicationCommandCallbackData in the
 // official documentation.
 type InteractionResponseData struct {
-	TTS             bool            `json:"tts"`
-	Content         string          `json:"content"`
-	Embeds          []discord.Embed `json:"embeds,omitempty"`
-	AllowedMentions AllowedMentions `json:"allowed_mentions,omitempty"`
+	TTS             bool                `json:"tts"`
+	Content         string              `json:"content"`
+	Components      []discord.Component `json:"components,omitempty"`
+	Embeds          []discord.Embed     `json:"embeds,omitempty"`
+	AllowedMentions AllowedMentions     `json:"allowed_mentions,omitempty"`
 }
 
 // RespondInteraction responds to an incoming interaction. It is also known as
 // an "interaction callback".
 func (c *Client) RespondInteraction(
 	id discord.InteractionID, token string, data InteractionResponse) error {
-
 	return c.FastRequest(
 		"POST",
 		EndpointInteractions+id.String()+"/"+token+"/callback",
