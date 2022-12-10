080c734b37ea6ef91be6da9291f1ce36f23e0830
diamondburned <diamond@arikawa-hi.me>
2022-12-10 06:02:37 -0800

    cmdroute: Add OverwriteCommands
    This adds a small helper function just for convenience.

---
+++ api/cmdroute/application.go
@@ -0,0 +1,28 @@
+package cmdroute
+
+import (
+	"github.com/diamondburned/arikawa/v3/api"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/pkg/errors"
+)
+
+// BulkCommandsOverwriter is an interface that allows to overwrite all commands
+// at once. Everything *api.Client will implement this interface, including
+// *state.State.
+type BulkCommandsOverwriter interface {
+	CurrentApplication() (*discord.Application, error)
+	BulkOverwriteCommands(appID discord.AppID, cmds []api.CreateCommandData) ([]discord.Command, error)
+}
+
+var _ BulkCommandsOverwriter = (*api.Client)(nil)
+
+// OverwriteCommands overwrites all commands for the current application.
+func OverwriteCommands(client BulkCommandsOverwriter, cmds []api.CreateCommandData) error {
+	app, err := client.CurrentApplication()
+	if err != nil {
+		return errors.Wrap(err, "cannot get current app ID")
+	}
+
+	_, err = client.BulkOverwriteCommands(app.ID, cmds)
+	return errors.Wrap(err, "cannot overwrite commands")
+}
