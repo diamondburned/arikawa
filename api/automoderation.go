5ac59ad300e1c113bfa99d7cf868b05079647505
hi117 <zacharyw09264@gmail.com>
2024-08-06 00:31:23 -0400

    Update docstrings and change how data is passed in

--- api/automoderation.go
+++ api/automoderation.go
@@ -5,7 +5,7 @@ import (
 	"github.com/diamondburned/arikawa/v3/utils/httputil"
 )
 
-// Get a list of all rules currently configured for the guild. Returns a list of auto moderation rule objects for the given guild.
+// ListAutoModerationRules gets a list of all rules currently configured for the guild. Returns a list of auto moderation rule objects for the given guild.
 //
 // This endpoint requires the MANAGE_GUILD permission.
 func (c *Client) ListAutoModerationRules(guildID discord.GuildID) ([]discord.AutoModerationRule, error) {
@@ -16,7 +16,7 @@ func (c *Client) ListAutoModerationRules(guildID discord.GuildID) ([]discord.Aut
 	)
 }
 
-// Get a single rule. Returns an auto moderation rule object.
+// GetAutoModerationRule gets a single rule. Returns an auto moderation rule object.
 //
 // This endpoint requires the MANAGE_GUILD permission.
 func (c *Client) GetAutoModerationRule(guildID discord.GuildID, ruleID discord.AutoModerationRuleID) (discord.AutoModerationRule, error) {
@@ -27,7 +27,7 @@ func (c *Client) GetAutoModerationRule(guildID discord.GuildID, ruleID discord.A
 	)
 }
 
-// Create a new rule. Returns an auto moderation rule on success. Fires an Auto Moderation Rule Create Gateway event.
+// CreateAutoModerationRule creates a new rule. Returns an auto moderation rule on success. Fires an Auto Moderation Rule Create Gateway event.
 //
 // This endpoint requires the MANAGE_GUILD permission.
 //
@@ -40,33 +40,38 @@ func (c *Client) CreateAutoModerationRule(guildID discord.GuildID, rule discord.
 }
 
 type ModifyAutoModerationRuleData struct {
+	GuildID discord.GuildID
+	RuleID  discord.AutoModerationRuleID
+	Rule    discord.AutoModerationRule
 	AuditLogReason
 }
 
-// Modify an existing rule. Returns an auto moderation rule on success. Fires an Auto Moderation Rule Update Gateway event.
+// ModifyAutoModerationRule modifies an existing rule. Returns an auto moderation rule on success. Fires an Auto Moderation Rule Update Gateway event.
 //
 // Requires MANAGE_GUILD permissions.
 //
 // All parameters for this endpoint are optional.
 //
 // This endpoint supports the X-Audit-Log-Reason header.
-func (c *Client) ModifyAutoModerationRule(guildID discord.GuildID, ruleID discord.AutoModerationRuleID, rule discord.AutoModerationRule, data ModifyAutoModerationRuleData) (*discord.AutoModerationRule, error) {
+func (c *Client) ModifyAutoModerationRule(data ModifyAutoModerationRuleData) (*discord.AutoModerationRule, error) {
 	var ret *discord.AutoModerationRule
-	return ret, c.RequestJSON(&ret, "PATCH", EndpointGuilds+guildID.String()+"/auto-moderation/rules/"+ruleID.String(),
-		httputil.WithJSONBody(rule),
+	return ret, c.RequestJSON(&ret, "PATCH", EndpointGuilds+data.GuildID.String()+"/auto-moderation/rules/"+data.RuleID.String(),
+		httputil.WithJSONBody(data.Rule),
 		httputil.WithHeaders(data.Header()),
 	)
 }
 
 type DeleteAutoModerationRuleData struct {
+	GuildID discord.GuildID
+	RuleID  discord.AutoModerationRuleID
 	AuditLogReason
 }
 
-// Delete a rule. Returns a 204 on success. Fires an Auto Moderation Rule Delete Gateway event.
+// DeleteAutoModerationRule deletes a rule. Returns a 204 on success. Fires an Auto Moderation Rule Delete Gateway event.
 //
 // This endpoint requires the MANAGE_GUILD permission.
 //
 // This endpoint supports the X-Audit-Log-Reason header.
-func (c *Client) DeleteAutoModerationRule(guildID discord.GuildID, ruleID discord.AutoModerationRuleID, data DeleteAutoModerationRuleData) error {
-	return c.FastRequest("DELETE", EndpointGuilds+guildID.String()+"/auto-moderation/rules/"+ruleID.String(), httputil.WithHeaders(data.Header()))
+func (c *Client) DeleteAutoModerationRule(data DeleteAutoModerationRuleData) error {
+	return c.FastRequest("DELETE", EndpointGuilds+data.GuildID.String()+"/auto-moderation/rules/"+data.RuleID.String(), httputil.WithHeaders(data.Header()))
 }
