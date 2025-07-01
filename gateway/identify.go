8d96dc3562113cf651c4cc366330df0a271f53a4
ayn2op <ayn2op@gmail.com>
2025-06-30 20:03:40 -0400

    feat(gateway): add new fields to IdentifyProperties
    Reference: https://docs.discord.food/reference#client-properties

--- gateway/identify.go
+++ gateway/identify.go
@@ -182,12 +182,15 @@ type IdentifyProperties struct {
 	Browser string `json:"browser"` // Arikawa
 	Device  string `json:"device"`  // Arikawa
 
-	// Optional
-	BrowserUserAgent string `json:"browser_user_agent,omitempty"`
-	BrowserVersion   string `json:"browser_version,omitempty"`
-	OSVersion        string `json:"os_version,omitempty"`
-	Referrer         string `json:"referrer,omitempty"`
-	ReferringDomain  string `json:"referring_domain,omitempty"`
+	// Optional (not applicable to bots)
+	BrowserUserAgent  string           `json:"browser_user_agent,omitempty"`
+	BrowserVersion    string           `json:"browser_version,omitempty"`
+	OSVersion         string           `json:"os_version,omitempty"`
+	Referrer          string           `json:"referrer,omitempty"`
+	ReferringDomain   string           `json:"referring_domain,omitempty"`
+	ClientBuildNumber string           `json:"client_build_number,omitempty"`
+	ReleaseChannel    string           `json:"release_channel,omitempty"`
+	SystemLocale      discord.Language `json:"system_locale,omitempty"`
 }
 
 // Shard is a type for two numbers that represent the Bot's shard configuration.
