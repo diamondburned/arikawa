52323fbb00ec540b4ccbb3efe09108bc7a56ef49
ayn2op <ayn2op@gmail.com>
2025-09-02 19:01:33 -0400

    feat(gateway): add {Bot,User}IdentifyProperties

--- gateway/identify.go
+++ gateway/identify.go
@@ -96,11 +96,61 @@ func (id *Identifier) QueryGateway(ctx context.Context) (gatewayURL string, err 
 
 // DefaultIdentity is used as the default identity when initializing a new
 // Gateway.
-var DefaultIdentity = IdentifyProperties{
+var DefaultIdentity = BotIdentifyProperties{
 	OS:      runtime.GOOS,
 	Browser: "Arikawa",
 	Device:  "Arikawa",
 }
+
+var (
+	_ IdentifyProperties = BotIdentifyProperties{}
+	_ IdentifyProperties = UserIdentifyProperties{}
+)
+
+type (
+	IdentifyProperties interface {
+		isIdentifyProperties()
+	}
+
+	BotIdentifyProperties struct {
+		// OS is the operating system of the client.
+		OS string `json:"os"`
+		// Browser is the browser the client is using.
+		Browser string `json:"browser"`
+		// Device is the model of the mobile device the client is running on.
+		Device string `json:"device"`
+	}
+
+	UserIdentifyProperties struct {
+		BotIdentifyProperties
+
+		OSVersion    string `json:"os_version,omitempty"`
+		OSSDKVersion string `json:"os_sdk_version,omitempty"`
+		OSArch       string `json:"os_arch,omitempty"`
+
+		BrowserVersion   string `json:"browser_version,omitempty"`
+		BrowserUserAgent string `json:"browser_user_agent,omitempty"`
+
+		ClientBuildNumber int                   `json:"client_build_number,omitempty"`
+		ClientEventSource option.NullableString `json:"client_event_source,omitempty"`
+		ClientLaunchID    string                `json:"client_launch_id,omitempty"`
+		ClientAppState    string                `json:"client_app_state,omitempty"`
+		IsFastConnect     bool                  `json:"is_fast_connect,omitempty"`
+
+		SystemLocale   discord.Language `json:"system_locale"`
+		HasClientMods  bool             `json:"has_client_mods,omitempty"`
+		ReleaseChannel string           `json:"release_channel,omitempty"`
+
+		Referrer        string `json:"referrer,omitempty"`
+		ReferrerCurrent string `json:"referrer_current,omitempty"`
+
+		ReferringDomain        string `json:"referring_domain,omitempty"`
+		ReferringDomainCurrent string `json:"referring_domain_current,omitempty"`
+	}
+)
+
+func (b BotIdentifyProperties) isIdentifyProperties()  {}
+func (u UserIdentifyProperties) isIdentifyProperties() {}
 
 // IdentifyCommand is a command for Op 2. It is the struct for a data that's
 // sent over in an Identify command.
@@ -174,26 +224,6 @@ func (i *IdentifyCommand) HasIntents(intents Intents) bool {
 	}
 
 	return Intents(*i.Intents).Has(intents)
-}
-
-type IdentifyProperties struct {
-	// Required
-	OS      string `json:"os"`      // GOOS
-	Browser string `json:"browser"` // Arikawa
-	Device  string `json:"device"`  // Arikawa
-
-	// Optional (not applicable to bots)
-	ClientBuildNumber     int              `json:"client_build_number,omitempty"`
-	BrowserUserAgent      string           `json:"browser_user_agent,omitempty"`
-	BrowserVersion        string           `json:"browser_version,omitempty"`
-	OSVersion             string           `json:"os_version,omitempty"`
-	Referrer              string           `json:"referrer,omitempty"`
-	ReferrerCurrent       string           `json:"referrer_current,omitempty"`
-	ReferrerDomainCurrent string           `json:"referrer_domain_current,omitempty"`
-	ReferringDomain       string           `json:"referring_domain,omitempty"`
-	ReleaseChannel        string           `json:"release_channel,omitempty"`
-	SystemLocale          discord.Language `json:"system_locale,omitempty"`
-	HasClientMods         bool             `json:"has_client_mods,omitempty"`
 }
 
 // Shard is a type for two numbers that represent the Bot's shard configuration.
