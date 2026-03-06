18e7078967b728f6cdfdae26b7990f9196a5c945
ayn2op <ayn2op@gmail.com>
2026-03-05 19:59:32 -0500

    Update discord/component.go
    Co-authored-by: Toπ <git@topi.wtf>

--- discord/component.go
+++ discord/component.go
@@ -1649,7 +1649,7 @@ type CheckboxGroupComponent struct {
 	// Whether the file upload requires files to be uploaded before submitting the modal (defaults to `true`)
 	Required bool `json:"required,omitempty"`
 	// The values of the selected options.
-	Values []string `json:"value,omitempty"`
+	Values []string `json:"values,omitempty"`
 }
 
 // Type implements the Component interface.
